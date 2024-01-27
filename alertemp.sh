52871a0fbd1797a606326a6a33bf9ba18f3ee8ba
Mauro <leproide@paranoici.org>
2024-01-27 01:14:13 +0100

    Update alertemp.sh

--- alertemp.sh
+++ alertemp.sh
@@ -10,7 +10,7 @@ threshold=80
 sensors | grep "Package id 0:" | while read -r line; do
     temp=$(echo "$line" | awk -F "+" '{ print $2 }' | awk -F "." '{ print $1 }');
     if ((temp > threshold)); then
-        message="PROXMOX ALERT for $(hostname). Temperature is $temp degrees"
+        message="TEMP ALERT for $(hostname). Temperature is $temp degrees"
         curl -s -d "chat_id=$telegramchatid&text=$message&parse_mode=markdown&disable_web_page_preview=1" "$url" > /dev/null 2>&1
     fi
 done
